0a09ad7309768dd44d25163e940db896f806732f
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-02-24 00:45:38 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,36 @@
 <template>
-  <h1>message: {{ message }}</h1>
-  <hr />
-  <h1>number: {{ number }}</h1>
-  <hr />
-  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
-  <hr />
-  <h1>number * 2 = {{ number * 2 }}</h1>
-  <hr />
-  <h1>{{ number > 150? 'number is greater than 150':'number is less than 150' }}</h1>
+  <h1>{{ message }}</h1>
+  <img src="/public/img/banner_1.jpg" alt="" />
+  <br />
 
-  <hr />
-  <h1 v-text="number"></h1>
-  <hr />
-
-  <h1>{{ harry }}</h1>
-  <h1>{{ harry.name }}</h1>
-  <hr />
-  <h1>{{ hogwartsWizards }}</h1>
-  <h1>{{ hogwartsWizards[0] }}</h1>
-  <hr />
-
-  <h1>{{ rawHtml }}</h1>
-  <h1 v-text="rawHtml"></h1>
-  <h1 v-html="rawHtml"></h1>
+  <button>Change image</button>
 
 </template>
 
 <script setup>
-let message = 'Hello, Vue!'
-let number = 50
+import { ref } from 'vue'
 
-function doubleNum(num) {
-  return num * 2
-}
+let message = 'Hello, v-bind!'
 
-let harry = {
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17, // Age during the final battle of Hogwarts
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
-}
-const hogwartsWizards = [
-  {
-    id: 1001,
-    name: 'Harry Potter',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Phoenix feather',
-      wood: 'Holly'
-    }
-  },
-  {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
-  },
-  {
-    id: 1003,
-    name: 'Ron Weasley',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Unicorn hair',
-      wood: 'Willow'
-    }
-  },
-  {
-    id: 1004,
-    name: 'Draco Malfoy',
-    house: 'Slytherin',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Hawthorn'
-    }
-  }
-]
-
-let rawHtml = '<span style="color: red">This should be red.</span>'
 
 
 </script>
 
+<style scoped>
+img {
+  max-width: 300px;
+}
 
-<style lang="scss" scoped></style>
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
+</style>
