f5697c8fac3ea0b82c0af18f979e43ac63ef9fa2
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-03-10 15:12:58 +0100

    28

---
+++ src/BaseLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header></header>
+        <!--We want header content here-->
+        <slot name="header"></slot>
+    
+        <main></main> 
+        <!--We want main content here-->
+        <slot name="main"></slot>
+
+        <footer></footer>
+        <!--We want footer content here-->
+        <slot name="footer"></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
